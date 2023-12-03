8e5d079b30dc4683b8f8a0f3001ac0d8eafc21d1
Sibo Wu <wusibo2020@hotmail.com>
2023-12-02 18:53:17 -0800

    fix miss commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
-
 import HelloWorld from "./Labs/a3/HelloWorld";
 import Labs from "./Labs";
-import Project from "./project";
+
 import Kanbas from "./Kanbas";
 import {HashRouter} from "react-router-dom";
 import {Routes, Route, Navigate} from "react-router";
 function App() {
   return (
     <HashRouter>
-    <div>
-      <Routes>
-        <Route path="/" element={<Navigate to="Kanbas" />} />
-        <Route path="/Hello" element={<HelloWorld />} />
-        <Route path="/Labs/*" element={<Labs />} />
-        <Route path="/Kanbas/*" element={<Kanbas />} />
-        <Route path="/Lectures" element={<StateManagement />} />
-      </Routes>
-      {/* {screen === "Hello" && <HelloWorld />}
-      {screen === "Labs" && <Labs />}
-      {screen === "Kanbas" && <Kanbas />} */}
-    </div>
-  </HashRouter>
+      <div>
+       
+         <Routes>
+            <Route path="/"         element={<Navigate to="/Labs"/>}/>
+            <Route path="/hello"    element={<HelloWorld/>}/>
+            <Route path="/Labs/*"   element={<Labs/>}/>
+            <Route path="/Kanbas/*" element={<Kanbas/>}/>
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
 export default App;
-
 
